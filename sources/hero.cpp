b7cedce246b3956104ea6b49d833cbbc85f87982
foreyoon <foreyoon@naver.com>
2014-11-05 18:21:49 +0900

    hero

--- sources/hero.cpp
+++ sources/hero.cpp
@@ -1,60 +1,113 @@
 #include <iostream>
-#include "global.hpp"
-#include "hero.hpp"
-
-#ifndef EMBIND_H
-#define EMBIND_H
-#include <emscripten/bind.h>
-#endif
-
-using namespace emscripten;
-
-hero::hero(sexuality sex) {
-	this->sex = sex;
-	if(sex == sexuality::man){
-		set_love(10);
-		set_relationship(30);
-		set_self_develop(30);
-		set_study(100);
-		set_MAX_ENERGY(100);
-		set_MAX_RELATIONSHIP(100);
-
+#include "global.h"
+#include "hero.h"
+class hero{
+public:
+	hero::hero(float max_love, float max_energy, float max_relationship, float max_selfdevelop, int sex_number, float max_study);
+	void change_love(float);
+	void change_energy(float);
+	void change_relationship(float);
+	void change_self_develop(float);
+	void change_study(float);
+	void change_title(float*);
+	void change_MAX_ENERGY(const float);
+	void change_MAX_LOVE(const float);
+	void change_MAX_RELATIONSHIP(const float);
+	void change_MAX_SELF_DEVELOP(const float); //status 변화시키는 method
+	float get_MAX_ENERGY();
+	float get_MAX_LOVE();
+	float get_MAX_RELATIONSHIP();
+	float get_MAX_SELF_DEVELOP();
+	float get_study();
+	float get_self_develop();
+	float get_relationship();
+	float get_energy();
+	float get_love(); //status return 받는 method
+private:
+	const float MAX_ENERGY;
+	const float MAX_LOVE;
+	const float MAX_RELATIONSHIP;
+	const float MAX_SELF_DEVELOP;
+	const float MAX_STUDY;
+	float energy;
+	float love;
+	float relationship;
+	float self_develop;
+	float study;
+	enum sexuality sex;
+	float title[5][5];
+	bool energy_is_zero; //에너지가 0인지 아닌지 구별함. 만약 체력이 0 일경우 다른 행동을 하지 못함
+};
+hero::hero(float max_love, float max_energy, float max_relationship, float max_selfdevelop, int sex_number, float max_study) : 
+//const 변수 초기화
+MAX_LOVE(max_love),MAX_ENERGY(max_energy),MAX_RELATIONSHIP(max_relationship), MAX_SELF_DEVELOP(max_selfdevelop),MAX_STUDY(max_study) { 
+	if(sex_number ==1){
+		sex = man;
+		love = 10;
+		relationship = 30;
+		self_develop = 30;
+		study = 100;
+		//성별이 남자일 때
 	}
-	else if(sex == sexuality::woman){
+	else if(sex_number == 2){
 		sex = woman;
-		set_love(100);
-		set_relationship(30);
-		set_self_develop(30);
-		set_study(100);
-		set_MAX_ENERGY(80);
-		set_MAX_RELATIONSHIP(90);
-	}
-	set_MAX_SELF_DEVELOP(100);
-	set_MAX_LOVE(100);
+		love = 100;
+		relationship = 30;
+		self_develop = 30;
+		study = 100;
+		}
+	//성별이 여자일 때
 	for(int i =0; i<5;i++){
 		for(int j = 0;j<5;j++){
 			title[i][j] = 0;
 		}
 	}
+	energy_is_zero = 0;
 }
-
-
-void hero::set_love(float love_){
-	love = love_;
+//모든 title 초기화
+//각종 status 변화를 구현함. 이 때, MAX치보다 크면 MAX치로 되고, 0보다 작아지면 0으로 초기화
+void hero::change_love(float love_){ // love status 변화
+	love =+ love_;
+	if(love > MAX_LOVE) 
+		love = MAX_LOVE;
+	else if(love<0)
+		love = 0;
 }
-void hero::set_energy(float energy_){
-	energy = energy_;
+void hero::change_energy(float energy_){ //energy status 변화
+	energy_is_zero = 0; //energy_is_zero가 0 이 아니도록 만들고 method 끝내기 전에 energy가 0보다 작거나 같으면 energy_is_zero 를 1로 변경
+	energy =+ energy_;
+	if(energy > MAX_ENERGY)
+		energy = MAX_LOVE;
+	else if(energy<=0){
+		energy = 0;
+		energy_is_zero = 1;
+	}
 }
-void hero::set_relationship(float relationship_){
-	relationship=relationship_;
+void hero::change_relationship(float relationship_){ //relationship status 변화
+	relationship =+ relationship_;
+	if(relationship > MAX_RELATIONSHIP)
+		relationship = MAX_RELATIONSHIP;
+	else if(relationship<0){
+		relationship = 0;
+	}
 }
-void hero::set_self_develop(float self_develop_){
-	self_develop = self_develop_;
+void hero::change_self_develop(float self_develop_){
+	self_develop =+ self_develop_;
+	if(self_develop > MAX_SELF_DEVELOP)
+		self_develop = MAX_SELF_DEVELOP;
+	else if(self_develop<0){
+		self_develop = 0;
+	}
 }
-void hero::set_study(float study_){
-	study = study_;
+void hero::change_study(float study_){
+	study =+ study_;
+	if(study>MAX_STUDY)
+		study = MAX_STUDY;
+	else if(study<0){
+		study = 0;
+	}
 }
-void hero::set_title(float* new_title){
+void hero::change_title(float* new_title){
 	int i;
 	for(i=0;i<5;i++){
 		if(title[0][i]==0){
@@ -68,47 +121,35 @@ void hero::set_title(float* new_title){
 
 
 
-float hero::get_study() const {
+float hero::get_study(){
 	return study;
 }
-float hero::get_self_develop() const {
+float hero::get_self_develop(){
 	return self_develop;
 }
-float hero::get_relationship() const {
+float hero::get_relationship(){
 	return relationship;
 }
-float hero::get_energy() const {
+float hero::get_energy(){
 	return energy;
 }
-float hero::get_love() const {
+float hero::get_love(){
 	return love;
 }
 
-void hero::set_MAX_ENERGY(float value){
-	MAX_ENERGY = value;
-}
-void hero::set_MAX_LOVE(float value){
-	MAX_LOVE = value;
-}
-void hero::set_MAX_RELATIONSHIP(float value){
-	MAX_RELATIONSHIP = value;
-}
-void hero::set_MAX_SELF_DEVELOP(float value){
-	MAX_SELF_DEVELOP = value;
-}
-
-float hero::get_MAX_ENERGY() const {
+float hero::get_MAX_ENERGY(){
 	return MAX_ENERGY;
 }
-float hero::get_MAX_LOVE() const {
+float hero::get_MAX_LOVE(){
 	return MAX_LOVE;
 }
-float hero::get_MAX_RELATIONSHIP() const {
+float hero::get_MAX_RELATIONSHIP(){
 	return MAX_RELATIONSHIP;
 }
-float hero::get_MAX_SELF_DEVELOP() const {
+float hero::get_MAX_SELF_DEVELOP(){
 	return MAX_SELF_DEVELOP;
 }
+
 
 EMSCRIPTEN_BINDINGS(PostechBegins_Hero) {
 	class_<hero>("Hero")
--- sources/hero.hpp
+++ sources/hero.hpp
@@ -2,41 +2,43 @@
 #define HERO
 
 #include <iostream>
-
+#include "global.h"
+#include "hero.h"
 class hero{
 public:
-	hero(sexuality sex);
-	void set_love(float);
-	void set_energy(float);
-	void set_relationship(float);
-	void set_self_develop(float);
-	void set_study(float);
-	void set_title(float*);
-	void set_MAX_ENERGY(float);
-	void set_MAX_LOVE(float);
-	void set_MAX_RELATIONSHIP(float);
-	void set_MAX_SELF_DEVELOP(float);
-	float get_MAX_ENERGY() const;
-	float get_MAX_LOVE() const;
-	float get_MAX_RELATIONSHIP() const;
-	float get_MAX_SELF_DEVELOP() const;
-	float get_study() const;
-	float get_self_develop() const;
-	float get_relationship() const;
-	float get_energy() const;
-	float get_love() const;
+	hero::hero(float max_love, float max_energy, float max_relationship, float max_selfdevelop, int sex_number, float max_study);
+	void change_love(float);
+	void change_energy(float);
+	void change_relationship(float);
+	void change_self_develop(float);
+	void change_study(float);
+	void change_title(float*);
+	void change_MAX_ENERGY(const float);
+	void change_MAX_LOVE(const float);
+	void change_MAX_RELATIONSHIP(const float);
+	void change_MAX_SELF_DEVELOP(const float); //status 변화시키는 method
+	float get_MAX_ENERGY();
+	float get_MAX_LOVE();
+	float get_MAX_RELATIONSHIP();
+	float get_MAX_SELF_DEVELOP();
+	float get_study();
+	float get_self_develop();
+	float get_relationship();
+	float get_energy();
+	float get_love(); //status return 받는 method
 private:
-	float MAX_ENERGY;
-	float MAX_LOVE;
-	float MAX_RELATIONSHIP;
-	float MAX_SELF_DEVELOP;
-
+	const float MAX_ENERGY;
+	const float MAX_LOVE;
+	const float MAX_RELATIONSHIP;
+	const float MAX_SELF_DEVELOP;
+	const float MAX_STUDY;
 	float energy;
 	float love;
 	float relationship;
 	float self_develop;
 	float study;
-	sexuality sex;
+	enum sexuality sex;
 	float title[5][5];
+	bool energy_is_zero; //에너지가 0인지 아닌지 구별함. 만약 체력이 0 일경우 다른 행동을 하지 못함
 };
 #endif
