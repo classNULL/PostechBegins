d22e142a0142f84b799881fec5f590eba18eb9dc
mmzar <mmzar@postech.ac.kr>
2014-11-17 20:45:17 +0900

    dfsdf

--- sources/cell.cpp
+++ sources/cell.cpp
@@ -1,17 +1,32 @@
 ﻿#include "cell.hpp"
-int normal::call_option(){
+void cell::change(hero* _hero,int study,int relationship,int self_develop, int love, int stress, int day){ 
+	_hero->change_energy();
+	_hero->change_study(study, day);
+	_hero->change_relationship(relationship, day);
+	_hero->change_self_develop(self_develop, day);
+	_hero->change_love(love, day);
+	_hero->change_stress(stress, day);
+}
+void normal::call_option(hero* _hero,int day);{
 	int option;
 	cout<<"1. 공부하기"<<endl;
 	cout<<"2. 술마시기"<<endl;
 	cout<<"3. 동아리 활동하기"<<endl;
 	cout<<"4. 연애하기"<<endl;
 	cout<<"5. 휴식"<<endl;
-	//칸에 맞는 옵션을 출력하여 사용자로 부터 옵션을 선택하도록 한다.
-	cin>>option;
-
-	return 10+option;
+	cin << option;
+	if (option == 1)//변화량이 0이라도 적어주는 이유는 칭호를 고려하였기 때문
+		change(_hero, 2, -2, 0, 0, 8, day);
+	else if (option == 2)
+		change(_hero, -4, 5, 0, 1, 5, day);
+	else if (option == 3)
+		change(_hero, -4, 5, 1, 1, -3, day);
+	else if (option == 4)
+		change(_hero, -4, -2, 0, 5, 0, day);
+	else if (option == 5)
+		change(_hero, 0, 0, 0, 0, -5, day);
 }
-int weekend::call_option(){
+void weekend::call_option(hero* _hero,int day);{
 	int option;
 	cout<<"1. 공부하기"<<endl;
 	cout<<"2. 술마시기"<<endl;
@@ -19,12 +34,9 @@ int weekend::call_option(){
 	cout<<"4. 연애하기"<<endl;
 	cout<<"5. 휴식"<<endl;
 	cout<<"6. 집가기"<<endl;
-	cin>>option;
-
-	return 10+option;
 }
 
-int march::call_option(){
+void march::call_option(hero* _hero,int day);{
 	int option;
 	cout<<"1. 공부하기"<<endl;
 	cout<<"2. 술마시기"<<endl;
@@ -32,70 +44,51 @@ int march::call_option(){
 	cout<<"4. 연애하기"<<endl;
 	cout<<"5. 휴식"<<endl;
 	cout<<"6. 밥사달라고하기"<<endl;
-	cin>>option;
-	if(option==6) option++;// option이 6일때 7로 바꿔줌
-	return 10+option;
 }
-int before_exam::call_option(){
+void before_exam::call_option(hero* _hero,int day);{
 	int option;
 	cout<<"1. 공부하기"<<endl;
 	cout<<"2. 술마시기"<<endl;
 
-	return 20+option
 }
-int exam::call_option(){
+void exam::call_option(hero* _hero,int day);{
 	return 30;
 }
 
-int after_exam::call_option(){
+void after_exam::call_option(hero* _hero,int day);{
 	int option;
 	cout<<"1. 공부하기"<<endl;
 	cout<<"2. 술마시기"<<endl;
 	cout<<"3. 연애하기"<<endl;
 
-	return 40+option;
 }
-int festival::call_option(){
+void festival::call_option(hero* _hero,int day);{
 	int option;
 	cout<<"1. 공부하기"<<endl;
 	cout<<"2. 술마시기"<<endl;
 	cout<<"3. 집에 가기"<<endl;
 
 
-	return 50+option;
 }
 
-int dance::call_option(){
+void dance::call_option(hero* _hero,int day);{
 	int option;
 	cout<<"1. 공부하기"<<endl;
 	cout<<"2. 술마시기"<<endl;
 	cout<<"3. 춤 연습하기"<<endl;
 
 
-	return 60+option;
 }
 
-int special::call_option(){
+void special::call_option(hero* _hero,int day);{
 	int option;
 	cout<<"1. 참가한다"<<endl;
 	cout<<"2. 참가하지 않는다"<<endl;
 
-	return 70+option;
 }
-//아래와 같이 사용
-select_option(_character,cell->call_option(),day);
 
 void GameCenter::select_option(hero* _hero,int option,int day){
-	if(option==11)//변화량이 0이라도 적어주는 이유는 칭호를 고려하였기 때문
-		_hero->change_energy(), _hero->change_study(2,day), _hero->change_relationship(-2,day), _hero->change_self_develop(0,day), _hero->change_love(0,day), _hero->change_stress(8,day);
-	else if(option==12)
-		_hero->change_energy(), _hero->change_study(-4,day), _hero->change_relationship(5,day), _hero->change_self_develop(0,day), _hero->change_love(1,day), _hero->change_stress(5,day);
-	else if(option==13)
-		_hero->change_energy(), _hero->change_study(-4,day), _hero->change_relationship(5,day), _hero->change_self_develop(1,day), _hero->change_love(1,day), _hero->change_stress(-3,day);
-	else if(option==14)
-		_hero->change_energy(), _hero->change_study(-4,day), _hero->change_relationship(-2,day), _hero->change_self_develop(0,day), _hero->change_love(5,day), _hero->change_stress(0,day);
-	else if(option==15)
-		_hero->change_energy(), _hero->change_study(0,day), _hero->change_relationship(0,day), _hero->change_self_develop(0,day), _hero->change_love(0,day), _hero->change_stress(-5,day);
+	
 	else if(option==16)
 		_hero->change_energy();
 	else if(option==17)
@@ -106,4 +99,3 @@ void GameCenter::select_option(hero* _hero,int option,int day){
 		_hero->change_energy(), _hero->change_study(2,day), _hero->change_relationship(-2,day), _hero->change_self_develop(0,day), _hero->change_love(0,day), _hero->change_stress(8,day);
 	//이하 구현 필요
 }
-void hero::change_study(int a, int day){study=study+(a+b)*day;}//b는 title로부터 오는 값
--- sources/cell.hpp
+++ sources/cell.hpp
@@ -3,49 +3,48 @@
 #include <iostream>
 using namespace std;
 
-//class saram;
 class cell{//한 칸의 정보로 Abstract class이다.
 public:
    virtual ~cell() {};
    virtual void call_option() = 0;//pure virtual method로 derived class에서 구현한다.
    bool stop_cell(){ return stop; }//무조건 멈추는 칸을 나타내기 위함으로, 멈춰야 하는 칸에서는 이 값이 true로 설정된다.
 protected:
-   //saram* saram;//character를 가리키는 포인터
    bool stop = false;
+   void change(hero* _hero, int study, int relationship, int self_develop, int love, int stress, int day);
 };
 class normal :public cell{//각각의 칸은 cell class를 상속받아 만들어진다.
 public:
-   void call_option();//캐릭터가 칸에 도달하였을때 선택지를 호출한다.
+   void call_option(hero* _hero,int day);//캐릭터가 칸에 도달하였을때 선택지를 호출한다.
 };
 class march :public cell{//3월을 나타내는 칸
 public:
-   void call_option();
+   void call_option(hero* _hero,int day);
 };
 class before_exam :public cell{//시험기간을 나타내는 칸
 public:
-   void call_option();
+   void call_option(hero* _hero,int day);
 };
 class exam :public cell{//중간고사, 기말고사를 나타내는 칸
 public:
    exam(){ stop = true; }
-   void call_option();
+   void call_option(hero* _hero,int day);
 };
 class after_exam :public cell{//시험기간 직후
 public:
-   void call_option();
+   void call_option(hero* _hero,int day);
 };
 class festival :public cell{//pop, 해맞이 한마당, 포카전 등 축제
 public:
    festival(){ stop = true; }
-   void call_option();
+   void call_option(hero* _hero,int day);
 };
 class dance :public cell{//학과춤 준비 기간
 public:
-   void call_option();
+   void call_option(hero* _hero,int day);
 };
 class special :public cell{//개총, 종총, MT 등 반드시 멈춰야하는 칸
 public:
    special(){ stop = true; }
-   void call_option();
+   void call_option(hero* _hero,int day);
 };
 #endif
