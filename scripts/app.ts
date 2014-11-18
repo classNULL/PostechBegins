296d80abf40bcb489968b9c9394649db3fabcf4a
SaschaNaz <saschanaz@outlook.com>
2014-11-19 05:26:05 +0900

    typescript

---
+++ scripts/app.ts
@@ -0,0 +1,48 @@
+﻿///<reference path="module.d.ts" />
+
+declare var selectGender: HTMLParagraphElement;
+declare var mover: HTMLParagraphElement;
+declare var characterImage: HTMLImageElement;
+declare var currentPositionRepresenter: HTMLSpanElement;
+
+Module.srand(Date.now() & 65535);
+var gameCenter: Module.GameCenter;
+
+function createGameCenter(gender: Module.Sexuality) {
+    gameCenter = new Module.GameCenter(gender);
+    var gamechar = gameCenter.character;
+    alert(
+        "love: " + gamechar.love
+        + "\r\nenergy: " + gamechar.energy
+        + "\r\nrelationship: " + gamechar.relationship
+        + "\r\nselfImprovement: " + gamechar.selfImprovement
+        + "\r\nstudy: " + gamechar.study
+        + "\r\nMAX_ENERGY: " + gamechar.MAX_ENERGY
+        + "\r\nMAX_LOVE: " + gamechar.MAX_LOVE
+        + "\r\nMAX_RELATIONSHIP: " + gamechar.MAX_RELATIONSHIP
+        + "\r\nMAX_SELFIMPROVEMENT: " + gamechar.MAX_SELFIMPROVEMENT);
+    selectGender.style.display = "none";
+    mover.style.display = "";
+
+    characterImage.style.display = "";
+    if (gender == Module.Sexuality.Man)
+        characterImage.src = "UI/캐릭터/남자/남자1.png";
+    else if (gender == Module.Sexuality.Woman)
+        characterImage.src = "UI/캐릭터/여자/여자1.png";
+}
+
+function move() {
+    var step = gameCenter.dice();
+    alert("주사위 수 " + step + "이(가) 나왔습니다.");
+    var position = gameCenter.move(step);
+    var posstr = dateIndexToString(position);
+    alert(position + "번 칸에 멈추었습니다. " + posstr + "입니다.");
+    currentPositionRepresenter.textContent = position + ". " + posstr;
+}
+
+function dateIndexToString(index: number) {
+    var monthday = new Module.MonthDay(index);
+    var result = monthday.month.value + "월 " + monthday.day + "일";
+    monthday.delete();
+    return result;
+}
---
+++ scripts/module.d.ts
@@ -0,0 +1,63 @@
+﻿declare module Module {
+    class EmscriptenClass {
+        delete(): void;
+    }
+    interface EmscriptenEnum {
+        value: number;
+    }
+}
+
+declare module Module {
+    function srand(seed: number): void;
+    class MonthDay extends EmscriptenClass {
+        constructor(dateIndex: number);
+        constructor(month: Month, day: number);
+
+        dateIndex: number;
+        month: Month;
+        day: number;
+    }
+    interface Month extends EmscriptenEnum {
+    }
+    module Month {
+        var March: Month;
+        var April: Month;
+        var May: Month;
+        var June: Month;
+        var July: Month;
+        var August: Month;
+        var September: Month;
+        var October: Month;
+        var November: Month;
+        var December: Month;
+    }
+    interface Sexuality extends EmscriptenEnum {
+    }
+    module Sexuality {
+        var Man: Sexuality;
+        var Woman: Sexuality;
+    }
+    class GameCenter extends EmscriptenClass {
+        constructor(sex: Sexuality);
+
+        date: number;
+        currentPosition: number;
+        character: Hero;
+
+        dice(): number;
+        score(study: number, relationship: number, improvement: number): number;
+        move(step: number): number;
+    }
+    class Hero extends EmscriptenClass {
+        constructor(maxLove: number, maxEnergy: number, maxRelationship: number, maxSelfImprovement: number, maxStudy: number, sex: Sexuality);
+        love: number;
+        energy: number;
+        relationship: number;
+        selfImprovement: number;
+        study: number;
+        MAX_ENERGY: number;
+        MAX_LOVE: number;
+        MAX_RELATIONSHIP: number;
+        MAX_SELFIMPROVEMENT: number;
+    }
+}
