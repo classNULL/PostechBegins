8b3b6933e4aac57aa62bb6d9d3bb4099665b234e
mmzar <mmzar@postech.ac.kr>
2014-11-23 22:20:09 +0900

    ads

--- sources/cell.cpp
+++ sources/cell.cpp
@@ -1,13 +1,13 @@
 ﻿#include "cell.hpp"
-void cell::change(hero* _hero,int study,int relationship,int self_develop, int love, int stress, int day){ 
-	_hero->change_energy();
+void cell::change(hero* _hero, int energy, float study, float relationship, float self_develop, float love, float stress, float day){
+	_hero->change_energy(energy,day);
 	_hero->change_study(study, day);
 	_hero->change_relationship(relationship, day);
 	_hero->change_self_develop(self_develop, day);
 	_hero->change_love(love, day);
 	_hero->change_stress(stress, day);
 }
-void normal::call_option(hero* _hero,int day);{
+void normal::call_option(hero* _hero,float day);{
 	int option;
 	cout<<"1. 공부하기"<<endl;
 	cout<<"2. 술마시기"<<endl;
@@ -16,15 +16,15 @@ void normal::call_option(hero* _hero,int day);{
 	cout<<"5. 휴식"<<endl;
 	cin << option;
 	if (option == 1)//변화량이 0이라도 적어주는 이유는 칭호를 고려하였기 때문
-		change(_hero, 2, -2, 0, 0, 8, day);
+		change(_hero,1, 2, -2, 0, 0, 8, day);
 	else if (option == 2)
-		change(_hero, -4, 5, 0, 1, 5, day);
+		change(_hero,1, -4, 5, 0, 1, 5, day);
 	else if (option == 3)
-		change(_hero, -4, 5, 1, 1, -3, day);
+		change(_hero,1, -4, 5, 1, 1, -3, day);
 	else if (option == 4)
-		change(_hero, -4, -2, 0, 5, 0, day);
+		change(_hero,1, -4, -2, 0, 5, 0, day);
 	else if (option == 5)
-		change(_hero, 0, 0, 0, 0, -5, day);
+		change(_hero,2, 0, 0, 0, 0, -5, day);
 }
 void weekend::call_option(hero* _hero,int day);{
 	int option;
@@ -33,7 +33,20 @@ void weekend::call_option(hero* _hero,int day);{
 	cout<<"3. 동아리 활동하기"<<endl;
 	cout<<"4. 연애하기"<<endl;
 	cout<<"5. 휴식"<<endl;
-	cout<<"6. 집가기"<<endl;
+	cout << "6. 집가기" << endl;
+	cin << option;
+	if (option == 1)//변화량이 0이라도 적어주는 이유는 칭호를 고려하였기 때문
+		change(_hero, 1, 2, -2, 0, 0, 8, day);
+	else if (option == 2)
+		change(_hero, 1, -4, 5, 0, 1, 5, day);
+	else if (option == 3)
+		change(_hero, 1, -4, 5, 1, 1, -3, day);
+	else if (option == 4)
+		change(_hero, 1, -4, -2, 0, 5, 0, day);
+	else if (option == 5)
+		change(_hero, 2, 0, 0, 0, 0, -5, day);
+	else
+		change(_hero, 3, 0, 0, 0, 0, 0, day);
 }
 
 void march::call_option(hero* _hero,int day);{
@@ -47,8 +60,22 @@ void march::call_option(hero* _hero,int day);{
 }
 void before_exam::call_option(hero* _hero,int day);{
 	int option;
-	cout<<"1. 공부하기"<<endl;
-	cout<<"2. 술마시기"<<endl;
+	cout << "1. 공부하기" << endl;
+	cout << "2. 술마시기" << endl;
+	cout << "3. 동아리 활동하기" << endl;
+	cout << "4. 연애하기" << endl;
+	cout << "5. 휴식" << endl;
+	cin << option;
+	if (option == 1)//변화량이 0이라도 적어주는 이유는 칭호를 고려하였기 때문
+		change(_hero, 1, 3, -1.6, 0, 0, 9.6, day);
+	else if (option == 2)
+		change(_hero, 1, -8, 6, 0, 1, 7.5, day);
+	else if (option == 3)
+		change(_hero, 1, -6, 6, 0.8, 1, -3.6, day);
+	else if (option == 4)
+		change(_hero, 1, -4, -2, 0, 5, 0, day);
+	else if (option == 5)
+		change(_hero, 2, -3, 0, 0, 0, -2.5, day);
 
 }
 void exam::call_option(hero* _hero,int day);{
@@ -57,45 +84,79 @@ void exam::call_option(hero* _hero,int day);{
 
 void after_exam::call_option(hero* _hero,int day);{
 	int option;
-	cout<<"1. 공부하기"<<endl;
-	cout<<"2. 술마시기"<<endl;
-	cout<<"3. 연애하기"<<endl;
+	cout << "1. 공부하기" << endl;
+	cout << "2. 술마시기" << endl;
+	cout << "3. 동아리 활동하기" << endl;
+	cout << "4. 연애하기" << endl;
+	cout << "5. 휴식" << endl;
+	cin << option;
+	if (option == 1)//변화량이 0이라도 적어주는 이유는 칭호를 고려하였기 때문
+		change(_hero, 1, 1.4, -4, 0, 0, 8, day);
+	else if (option == 2)
+		change(_hero, 1, -2, 7.5, 0, 1, 4, day);
+	else if (option == 3)
+		change(_hero, 1, -2, 6, 1, 1, -3, day);
+	else if (option == 4)
+		change(_hero, 1, -2, -3, 0, 5, 0, day);
+	else if (option == 5)
+		change(_hero, 2, 0, 0, 0, 0, -5, day);
 
 }
-void festival::call_option(hero* _hero,int day);{
+void festival::call_option(hero* _hero, int day); {
 	int option;
-	cout<<"1. 공부하기"<<endl;
-	cout<<"2. 술마시기"<<endl;
-	cout<<"3. 집에 가기"<<endl;
+	cout << "1. 공부하기" << endl;
+	cout << "2. 술마시기" << endl;
+	cout << "3. 연애하기" << endl;
+	cout << "4. 휴식" << endl;
+	cout << "5. 집가기" << endl;
+	cin << option;
+	if (option == 1)//변화량이 0이라도 적어주는 이유는 칭호를 고려하였기 때문
+		change(_hero, 1, 2, -10, 0, 0, 8, day);
+	else if (option == 2)
+		change(_hero, 1, -0.8, 15, 0, 1, 4, day);
+	else if (option == 3)
+		change(_hero, 1, -4, -12, 0, 5, 0, day);
+	else if (option == 4)
+		change(_hero, 2, 0, -10, 0, 0, -4, day);
+	else if (option == 5)
+		change(_hero, 3, 0, -12, 0, 0, 0, day);
 
 
 }
 
-void dance::call_option(hero* _hero,int day);{
+void dance::call_option(hero* _hero, int day); {
 	int option;
-	cout<<"1. 공부하기"<<endl;
-	cout<<"2. 술마시기"<<endl;
-	cout<<"3. 춤 연습하기"<<endl;
+	cout << "1. 공부하기" << endl;
+	cout << "2. 술마시기" << endl;
+	cout << "3. 동아리 활동하기" << endl;
+	cout << "4. 연애하기" << endl;
+	cout << "5. 휴식" << endl;
+	cout << "6. 춤 연습하기" << endl;
+	cin << option;
+	if (option == 1)//변화량이 0이라도 적어주는 이유는 칭호를 고려하였기 때문
+		change(_hero, 1, 2, -3, 0, 0, 8, day);
+	else if (option == 2)
+		change(_hero, 1, -4, 5, 0, 1, 5, day);
+	else if (option == 3)
+		change(_hero, 1, -4, 4, 1, 1, -3, day);
+	else if (option == 4)
+		change(_hero, 1, -4, -3, 0, 5, 0, day);
+	else if (option == 5)
+		change(_hero, 2, 0, -3, 0, 0, -5, day);
+	else if (option == 6)
+		change(_hero, 1, -4, 7, 0, 0, 6, day);
 
 
 }
 
-void special::call_option(hero* _hero,int day);{
+void special::call_option(hero* _hero, int day); {
 	int option;
-	cout<<"1. 참가한다"<<endl;
-	cout<<"2. 참가하지 않는다"<<endl;
+	cout << "1. 참가한다" << endl;
+	cout << "2. 참가하지 않는다" << endl;
 
-}
+	if (option == 1)//변화량이 0이라도 적어주는 이유는 칭호를 고려하였기 때문
+		change(_hero, 1, -4, 8, 0, 0, 4, day);
+	else if (option == 2)
+		change(_hero, 1, 4, -8, 0, 0, -2, day);
 
-void GameCenter::select_option(hero* _hero,int option,int day){
-	
-	else if(option==16)
-		_hero->change_energy();
-	else if(option==17)
-		//밥사달라고하기
-	else if(option==21)//시험기간이므로 세부수치 변경 필요
-		_hero->change_energy(), _hero->change_study(2,day), _hero->change_relationship(-2,day), _hero->change_self_develop(0,day), _hero->change_love(0,day), _hero->change_stress(8,day);
-	else if(option==22)
-		_hero->change_energy(), _hero->change_study(2,day), _hero->change_relationship(-2,day), _hero->change_self_develop(0,day), _hero->change_love(0,day), _hero->change_stress(8,day);
-	//이하 구현 필요
 }
