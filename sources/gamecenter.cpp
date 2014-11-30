5f255e00025ed95e8fb57f97c9fd69eb5eebb93e
Hwang-Gunwoo <gunwoo2512@naver.com>
2014-11-30 19:07:38 +0900

    Update gamecenter.cpp

--- sources/gamecenter.cpp
+++ sources/gamecenter.cpp
@@ -33,6 +33,13 @@ int GameCenter::score(float study, float relationship, float improvement) {
 int GameCenter::move(int step){
   int stop = this->_map.check_stop(this->_current_position, step);
   this->_current_position = stop;
+  
+  for(int i=0;i++;i<step)
+  {
+    if(_map.check_birth(this->_current_position, step)
+      cell::change(_hero, 2, 0, 0, 0, 0, -5, day);
+  }
+  
   return stop;
 }
 
