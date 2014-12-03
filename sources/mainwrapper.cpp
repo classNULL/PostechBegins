d6814066af7d611d52bfe91489ee065dbba0f825
SaschaNaz <saschanaz@outlook.com>
2014-12-03 23:34:17 +0900

    cell name

--- sources/mainwrapper.cpp
+++ sources/mainwrapper.cpp
@@ -40,7 +40,8 @@ EMSCRIPTEN_BINDINGS(PostechBegins_Main) {
     .function("at", &MapTable::at, allow_raw_pointers());
 
   class_<cell>("Cell")
-    .property("isStop", &cell::stop_cell);
+    .property("isStop", &cell::stop_cell)
+    .function("callOptions", &cell::call_option);
   class_<normal, base<cell>>("NormalCell");
   class_<weekend, base<cell>>("WeekendCell");
   class_<march, base<cell>>("MarchCell");
