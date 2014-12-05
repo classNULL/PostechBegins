3ebb7516ee1c792a719122a510cd41f4bf48ff5b
susungtamsa <susungtamsa@gmail.com>
2014-12-05 18:01:27 +0900

    Update event.hpp

--- sources/event.hpp
+++ sources/event.hpp
@@ -8,73 +8,73 @@ protected:
 	Month month;
 	bool successed = 0;
 public:
-	virtual bool check_success()=0;
+	virtual wstring check_success()=0;
 	Month get_month();
 	virtual void reward()=0;
-	virtual void introduction_message()=0;
+	virtual wstring introduction_message()=0;
 };
 
 class march_event : public month_event{
 protected:
 	Month month = Month::March;
 public:
-	void introduction_message();
-	bool success();
-	void check_success();
+	wstring introduction_message();
+
+	wstring check_success();
 };
 
 class april_event : public month_event{
 protected:
 	Month month = Month::April;
 public:
-	void introduction_message();
-	bool success();
+	wstring introduction_message();
+	wstring check_success();
 };
 
 class may_event : public month_event{
 protected:
 	Month month = Month::May;
 public:
-	void introduction_message();
-	bool success();
+	wstring introduction_message();
+	wstring check_success();
 };
 
 class june_event : public month_event{
 protected:
 	Month month = Month::June;
 public:
-	void introduction_message();
-	bool success();
+	wstring introduction_message();
+	wstring check_success();
 };
 
 class september_event : public month_event{
 protected:
 	Month month = Month::September;
 public:
-	void introduction_message();
-	bool success();
+	wstring introduction_message();
+	wstring check_success();
 };
 
 class october_event : public month_event{
 protected:
 	Month month = Month::October;
 public:
-	void introduction_message();
-	bool success();
+	wstring introduction_message();
+	wstring check_success();
 };
 
 class november_event : public month_event{
 protected:
 	Month month = Month::November;
 public:
-	void introduction_message();
-	bool success();
+	wstring introduction_message();
+	wstring check_success();
 };
 
 class december_event : public month_event{
 protected:
 	Month month = Month::December;
 public:
-	void introduction_message();
-	bool success();
+	wstring introduction_message();
+	wstring check_success();
 };
